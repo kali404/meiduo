99db2fb7e3c818b1c3e3d7cdd94d414d5ac0d364
王晓丰 <18730595738@163.com>
2019-04-22 22:29:41 +0800

    配置子应用 文件夹完成

--- MeiDuo_Store/MeiDuo_Store/apps/users/urls.py
+++ MeiDuo_Store/MeiDuo_Store/apps/users/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-
+from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('users.urls', namespace='users')),
+    url(r'^register/$', views.RegisterView.as_view(), name='register'),
 ]
---
+++ MeiDuo_Store/MeiDuo_Store/apps/users/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views import View
+
+
+class RegisterView(View):
+    """用户注册"""
+
+    def get(self, request):
+        """
+        提供注册界面
+        :param request: 请求对象
+        :return: 注册界面
+        """
+        return render(request, 'register.html')
