70ccf2f8ef312a16abf1145a725e3537dc2b8170
王晓丰 <18730595738@163.com>
2019-04-23 21:11:54 +0800

    02-用户注册基本验证

--- MeiDuo_Store/MeiDuo_Store/apps/users/urls.py
+++ MeiDuo_Store/MeiDuo_Store/apps/users/urls.py
@@ -17,6 +17,5 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^register/$', views.RegisterView.as_view(), name='register'),
 ]
--- MeiDuo_Store/MeiDuo_Store/apps/users/views.py
+++ MeiDuo_Store/MeiDuo_Store/apps/users/views.py
@@ -1,7 +1,13 @@
+import re
+
+from django.contrib.auth import login
 from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
+from .models import User
+
+from django.http import *
 
 
 class RegisterView(View):
@@ -10,7 +16,51 @@ class RegisterView(View):
     def get(self, request):
         """
         提供注册界面
-        :param request: 请求对象
-        :return: 注册界面
         """
         return render(request, 'register.html')
+
+    def post(self, request):
+        user_name = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        cpassworld = request.POST.get('cpwd')
+        phone = request.POST.get('phone')
+        allow = request.POST.get('allow')
+
+        if not all(['user_name', 'password', 'cpassworld', 'phone', 'allow']):
+            return HttpResponseForbidden('参数不完整')
+        if not re.match(r'^[A-Za-z0-9]{5,20}', user_name):
+            return HttpResponseForbidden('请输入5-20位用户名')
+        if not User.objects.filter(user_name=user_name):
+            return HttpResponseForbidden('用户名以存在')
+        if re.match(r'^[A-Za-z0-9]{5,20}',password):
+            return HttpResponseForbidden('请输入5-20位密码')
+        if cpassworld!=password:
+            return HttpResponseForbidden('密码输入不一致')
+        if not re.match(r'^1[345789]\d{9}$', phone):
+            return HttpResponseForbidden('请输入正确的手机号')
+        if not User.objects.filter(mobile=phone):
+            return HttpResponseForbidden('手机号以存在')
+
+        user = User.objects.create_user(username=user_name, password=password, mobile=phone)
+#         保持登陆状态
+        request.session['user_id'] = user.id
+#           django封装了login
+        login(request,user)
+
+        return HttpResponse('注册成功')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
